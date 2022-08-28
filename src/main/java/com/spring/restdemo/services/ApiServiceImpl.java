25833314994f14e9c81ab31ffcc8ac34029b39c7
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-28 18:38:35 +0530

    make api url global

--- src/main/java/com/spring/restdemo/services/ApiServiceImpl.java
+++ src/main/java/com/spring/restdemo/services/ApiServiceImpl.java
@@ -1,8 +1,14 @@
 package com.spring.restdemo.services;
 
 import com.spring.restdemo.api.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,10 +16,13 @@ import java.util.List;
 @Service
 public class ApiServiceImpl implements ApiService {
     private RestTemplate restTemplate;
+    private final String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
+
 
     @Override
     public List<User> getUsers(Integer limit) {
