36038f5c71dd10e1c09a66e1acef79325b1bcb68
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-28 18:35:17 +0530

    change getUsers to flux

--- src/main/java/com/spring/restdemo/services/ApiService.java
+++ src/main/java/com/spring/restdemo/services/ApiService.java
@@ -1,9 +1,11 @@
 package com.spring.restdemo.services;
 
 import com.spring.restdemo.api.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface ApiService {
-    List<User> getUsers(Integer limit);
+    Flux<User> getUsers(Mono<Integer> limit);
 }
