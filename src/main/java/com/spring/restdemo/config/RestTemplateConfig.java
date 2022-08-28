37e5f54625b6a805a122f257d6f8ee36367f6a47
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-28 19:03:07 +0530

    init rest template

--- src/main/java/com/spring/restdemo/config/RestTemplateConfig.java
+++ src/main/java/com/spring/restdemo/config/RestTemplateConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
-        return restTemplateBuilder.build();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
